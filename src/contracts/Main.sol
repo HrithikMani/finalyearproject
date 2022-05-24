a5214f0954bc0f44d25260f1b00cdb7792da0fd6
Hrithik Mani <hrithik.mani2000@gmail.com>
2022-05-24 15:55:13 +0530

    Basic test

--- src/contracts/Main.sol
+++ src/contracts/Main.sol
@@ -2,4 +2,14 @@ pragma solidity >=0.5.16;
 
 contract Main{
     string public name = "Hrithik";
+    //store posts
+    struct Image{
+        uint id;
+        string name;
+        string description;
+        address myacc;
+    }
+    mapping(uint => Image) public Images;
+
+    //create posts 
 }
